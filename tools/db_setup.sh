2f393fae4cc6c1eb282989fcd06117ed190c9d34
Nikita Zyuzin <nzyuzin93@gmail.com>
2017-04-16 03:01:13 +0200

    Check if database already exists before creating tables

--- tools/db_setup.sh
+++ tools/db_setup.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-sqlite3 rctf.db < tools/create_tables.sql
+if [ ! -e rctf.db ]; then
+  sqlite3 rctf.db < tools/create_tables.sql
+else
+  echo "Database already exists!"
+  exit 1
+fi
