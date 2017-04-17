e257185921d1faef9d830669092de593cdacb266
Nikita Zyuzin <nzyuzin93@gmail.com>
2017-04-17 13:31:50 +0200

    Add clans view

--- tools/create_tables.sql
+++ tools/create_tables.sql
@@ -3,3 +3,9 @@ create table players(
   clan varchar(11) NOT NULL,
   rating int NOT NULL
 );
+
+create view clans as
+select clan, cast(avg(players.rating) as int) as clan_rating
+from players
+group by clan
+order by clan_rating desc;
