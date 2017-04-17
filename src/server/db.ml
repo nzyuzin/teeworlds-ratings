fd5e3a510df86bf3817a7a254a0546893a980be7
Nikita Zyuzin <nzyuzin93@gmail.com>
2017-04-18 01:09:24 +0200

    Store game information in the database

--- src/server/db.ml
+++ src/server/db.ml
@@ -1,5 +1,3 @@
-type player = {name: string; clan: string; rating: int64}
-
 let db = Global.empty "db"
 
 let open_db db_name =
@@ -10,7 +8,11 @@ let close_db () =
   | true -> ()
   | false -> raise (Failure "Couldn't close the database!")
 
-let insert_player_stmt = "insert into players values (?, ?, ?)"
+(* Players *)
+
+type player = {name: string; clan: string; rating: int64}
+
+let insert_player_stmt = "insert into players (name, clan, rating) values (?, ?, ?)"
 
 let select_player_stmt = "select name, clan, rating from players where name = ?"
 
@@ -24,7 +26,7 @@ let insert_player (player: player) =
   let _ = bind prepared_insert_stmt 3 (Data.INT (Int64.of_int 1500)) in
   let _ = step prepared_insert_stmt in
   let _ = finalize prepared_insert_stmt in
-  ()
+  last_insert_rowid (Global.get db)
 
 let select_player (player_name: string): player option =
   let open Sqlite3 in
@@ -50,3 +52,37 @@ let update_rating (player_name: string) (new_rating: int64): unit =
   let _ = step prepared_update_stmt in
   let _ = finalize prepared_update_stmt in
   ()
+
+(* Games *)
+
+let insert_game_stmt =
+  "insert into games (gametype, map, game_time, winner, game_date) " ^
+  "values (?, ?, ?, ?, datetime('now'))"
+
+let insert_game_player_stmt =
+  "insert into game_players (game_id, player_id, score, team) " ^
+  "select ? as game_id, id as player_id, ? as score, ? as team from players where name = ?"
+
+let insert_game (game: Gameinfo.gameinfo) =
+  let open Sqlite3 in
+  let prepared_insert_stmt = prepare (Global.get db) insert_game_stmt in
+  let _ = bind prepared_insert_stmt 1 (Data.TEXT game.Gameinfo.gametype) in
+  let _ = bind prepared_insert_stmt 2 (Data.TEXT game.Gameinfo.map) in
+  let _ = bind prepared_insert_stmt 3 (Data.INT (Int64.of_int game.Gameinfo.time)) in
+  let winner = Gameinfo.string_of_team game.Gameinfo.winner in
+  let _ = bind prepared_insert_stmt 4 (Data.TEXT winner) in
+  let _ = step prepared_insert_stmt in
+  let _ = finalize prepared_insert_stmt in
+  last_insert_rowid (Global.get db)
+
+let insert_game_player (player: Gameinfo.player) game_id =
+  let open Sqlite3 in
+  let prepared_insert_stmt = prepare (Global.get db) insert_game_player_stmt in
+  let _ = bind prepared_insert_stmt 1 (Data.INT game_id) in
+  let _ = bind prepared_insert_stmt 2 (Data.INT (Int64.of_int player.Gameinfo.score)) in
+  let team = Gameinfo.string_of_team player.Gameinfo.team in
+  let _ = bind prepared_insert_stmt 3 (Data.TEXT team) in
+  let _ = bind prepared_insert_stmt 4 (Data.TEXT player.Gameinfo.name) in
+  let _ = step prepared_insert_stmt in
+  let _ = finalize prepared_insert_stmt in
+  last_insert_rowid (Global.get db)
--- src/server/server.ml
+++ src/server/server.ml
@@ -4,19 +4,23 @@ let db_player_of_gameinfo_player: Gameinfo.player -> Db.player = function
   {Gameinfo.name = nm; Gameinfo.clan = cn; Gameinfo.score = scr; Gameinfo.team = tm} ->
     {Db.name = nm; Db.clan = cn; Db.rating = Int64.of_int (-1)}
 
-let process_player_info player gameinfo =
-  match Db.select_player player.Gameinfo.name with
-  | None -> Db.insert_player (db_player_of_gameinfo_player player)
+let process_player_info player gameinfo game_id =
+  let _ = match Db.select_player player.Gameinfo.name with
+  | None -> let _ = Db.insert_player (db_player_of_gameinfo_player player) in
+    ()
   | Some existing_player ->
       let new_rating = calculate_new_rating existing_player gameinfo in
-      Db.update_rating existing_player.Db.name new_rating
+      Db.update_rating existing_player.Db.name new_rating in
+  let _ = Db.insert_game_player player game_id in
+  ()
 
 let process_message (msg: string) (db: string): unit =
   let json = Json.json_of_string msg in
   let gameinfo = Json.gameinfo_of_json json in
   let players = gameinfo.Gameinfo.players in
   let _ = Db.open_db db in
-  let _ = List.iter (fun player -> process_player_info player gameinfo) players in
+  let game_id = Db.insert_game gameinfo in
+  let _ = List.iter (fun player -> process_player_info player gameinfo game_id) players in
   Db.close_db ()
 
 let handle_connection (conn: Network.connection) (db: string): unit =
