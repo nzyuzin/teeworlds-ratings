4d2316b1e366f76bf3fdfb897e42925b0fc39e1b
Nikita Zyuzin <nzyuzin93@gmail.com>
2017-04-19 13:38:42 +0200

    Implement elo function

--- src/server/server.ml
+++ src/server/server.ml
@@ -1,15 +1,30 @@
-let calculate_new_rating player gameinfo = Int64.of_int 1500
-
 let db_player_of_gameinfo_player: Gameinfo.player -> Db.player = function
   {Gameinfo.name = nm; Gameinfo.clan = cn; Gameinfo.score = scr; Gameinfo.team = tm} ->
     {Db.name = nm; Db.clan = cn; Db.rating = Int64.of_int (-1)}
+
+type game_result = Victory | Defeat
+
+let score_of_game_result = function
+  | Victory -> 1.
+  | Defeat -> 0.
+
+let elo (rating_a: int64) (rating_b: int64) (game_result: game_result): int64 =
+  let a = Int64.to_float rating_a in
+  let b = Int64.to_float rating_b in
+  let expected_score = 1. /. (1. +. (10. ** ((b -. a) /. 400.))) in
+  let k = if a >= 2000. then 10. else 20. in
+  Int64.of_float (a +. (k *. ((score_of_game_result game_result) -. expected_score)))
+
+let calculate_average_rating (players: Db.player list): int64 =
+  let ratings = List.map (fun player -> player.Db.rating) players in
+  List.fold_left ( Int64.add ) (Int64.of_int 0) ratings
 
 let process_player_info player gameinfo game_id =
   let _ = match Db.select_player player.Gameinfo.name with
   | None -> let _ = Db.insert_player (db_player_of_gameinfo_player player) in
     ()
   | Some existing_player ->
-      let new_rating = calculate_new_rating existing_player gameinfo in
+      let new_rating = elo existing_player.Db.rating existing_player.Db.rating Victory in
       Db.update_rating existing_player.Db.name new_rating in
   let _ = Db.insert_game_player player game_id in
   ()
